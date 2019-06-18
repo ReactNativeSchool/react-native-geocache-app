db3e45518880837a50f920481c778c6425521929
Spencer Carli <spencercarli@gmail.com>
2019-06-18 10:09:41 -0500

    05 Database setup

--- server/api/geocache/index.js
+++ server/api/geocache/index.js
@@ -1,9 +1,27 @@
 const app = require('../../util/configureApi');
+const Cache = require('../../models/Cache');
+const connectDB = require('../../util/db');
 
 app.post('*', (req, res) => {
-  res.status(200).json({
-    result: {},
-  });
+  connectDB()
+    .then(() => {
+      return Cache.create({
+        title: 'Test',
+        // description: 'This is a description',
+        // latitude: 123,
+        // longitude: 123,
+      });
+    })
+    .then((cacheItem) => {
+      res.status(200).json({
+        result: cacheItem,
+      });
+    })
+    .catch(error => {
+      res.status(error.statusCode || 500).json({
+        error: error.message,
+      });
+    });
 });
 
 module.exports = app;
---
+++ server/util/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+let isConnected;
+
+const DB_URL = 'mongodb+srv://admin:HBLDemo@cluster0-5eefd.mongodb.net/test?retryWrites=true&w=majority';
+
+const connectToDatabase = () => {
+  if (isConnected) {
+    console.log('Use existing database connection.');
+    return Promise.resolve();
+  }
+
+  console.log('Using new database connection.')
+  return mongoose.connect(DB_URL, { useNewUrlParser: true }).then(db => {
+    isConnected = db.connections[0].readyState;
+  });
+}
+
+module.exports = connectToDatabase;
